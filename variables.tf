ac4e0f0730902a23af458a08978347488017eddc
BetterCyber <78450870+bettercyber@users.noreply.github.com>
2023-09-01 09:09:21 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "kinesis_stream_name" {
+  description = "Name of the Kinesis Stream"
+}
+
+variable "region" {
+  description = "AWS region"
+}
+
+variable "user_name" {
+  description = "Blumira user name"
+}
+
+variable "namespace" {
+  description = "Namespace"
+}
